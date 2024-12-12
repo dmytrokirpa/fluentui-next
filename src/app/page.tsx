d7b6796d3146631cee98ebbf5b7228f3da3ed750
Dmytro Kirpa <dmytrokirpa@microsoft.com>
2024-12-12 16:04:40 +0100

    react-18

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { BookmarkRegular } from "@fluentui/react-icons";
 export default function Index() {
   return (
     <Card style={{ maxWidth: "400px", margin: "20px" }}>
-      <Title1>Fluent UI + Next.js 15 & React 19</Title1>
+      <Title1>Fluent UI + Next.js 15 & React 18</Title1>
       <Body1>Welcome to your new app!</Body1>
       <Button appearance="primary" icon={<BookmarkRegular />} onClick={() => alert('It works!')}>
         Click me
